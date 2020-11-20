a704377726a866a87ee71295c6ef02d442a118ee
Murat Sahin <yazilimogren1111@gmail.com>
2020-11-20 22:43:11 +0100

    In About Page Route and Link Added

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,36 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory, Switch, BrowserRouter, Link, Route } from "react-router-dom";
 import { StyledAbout, StyledAboutWrapper } from "./About.style";
+
+function Page1() {
+  return <h1>You are on Page1</h1>
+}
+function Page2() {
+  return <h1>You are on Page2</h1>
+}
+function Page3() {
+  return <h1>You are on Page3</h1>
+}
 
 export const About = () => {
   const history = useHistory();
+
   return (
     <StyledAboutWrapper onClick={() => history.push("/hahahay")}>
+      <BrowserRouter>
+        <Link to='/page1'>Page_1</Link>
+        <Link to='/page2'>Page_2</Link>
+        <Link to='/page3'>Page_3</Link>
+        <Switch>
+          <Route path='/page1' component={Page1} />
+          <Route path='/page2' component={Page2} />
+          <Route path='/page3' component={Page3} />
+        </Switch>
+      </BrowserRouter>
+
+
+
+
       <StyledAbout>I am creating this website to learn it better.</StyledAbout>
       <StyledAbout>
         {" "}
@@ -14,6 +39,8 @@ export const About = () => {
       <StyledAbout>
         Bu sayfayi kendimi gelistirmek icin olusturuyorum.
       </StyledAbout>
+
     </StyledAboutWrapper>
   );
 };
+
